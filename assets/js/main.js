070bbc542ebcc74330036dca6d0fd4144972450a
Ali <aliclowd13@gmail.com>
2019-09-07 21:49:16 +0330

    Starbucks UI

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// swipe footer items
+$('.footer-swiper-items').slick({
+  centerMode: true,
+  centerPadding: '100px',
+  slidesToShow: 3,
+  accessibility: false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+// credit
+console.log("%c hey ! \n you can't find anything here for sure \n but nice try :) \n %c credit: Ali clowd \u00A9 2019 \n contact with me: aliclowd13@gmail.com",
+"color:#da0808;font-size:30px",
+"color:#1a7bc1;font-size:18px;")
+
